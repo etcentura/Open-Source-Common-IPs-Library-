7611e45740cc06c0d2a44898e77dad328571b507
unknown <etcentura@mail.ru>
2026-07-07 20:18:41 +0300

    hard. video test pattern generator ready for the testing

--- common_ip_video_pattern_generator/sv_src/video_pattern_generator_wrapper.sv
+++ common_ip_video_pattern_generator/sv_src/video_pattern_generator_wrapper.sv
@@ -2,14 +2,12 @@ module video_pattern_generator_wrapper
 #
 (
     parameter   int     SYS_REG_WIDTH           =	32                                  ,   // Width of setup registers
-    parameter   int     OUT_DATA_WIDTH          =	16                                  ,   // Width of the data output
-    parameter   bit     IS_RST_SYNC             =   1'b0                                    // Use comb or seq logic: "1" - sync rst_n, "0" - async
-
+    parameter   int     OUT_DATA_WIDTH          =	16                                      // Width of the data output
 )
 (
     //Basic signals declaration 
     input		logic		                                clk                         ,   // Basic clk signal
-    
+
     //Enable signal
     input		logic		                                enable                      ,   // Enable signal to generate pattern, when 0 - generation restarts and registers 
 
@@ -20,9 +18,13 @@ module video_pattern_generator_wrapper
     input		logic		[SYS_REG_WIDTH - 1 : 0  ]       requested_rows_del_before   ,   // Requested number of rows to be delayed
     input		logic		[SYS_REG_WIDTH - 1 : 0  ]       requested_cols_del_after    ,   // Requested number of colomns to be delayed
     input		logic		[SYS_REG_WIDTH - 1 : 0  ]       requested_rows_del_after    ,   // Requested number of rows to be delayed
-    input		logic		[SYS_REG_WIDTH - 1 : 0  ]       requested_rows_del_after    ,   // Requested number of rows to be delayed
     input		logic		[SYS_REG_WIDTH - 1 : 0  ]       requested_v_sync_duration   ,   // Number of ticks to hold vsync active high
     input		logic		[3                 : 0  ]       requested_data_pattern      ,   // Requested pattren type to be generated
+    input       logic       [SYS_REG_WIDTH - 1 : 0  ]       requested_level_white       ,   // Requested level of the white color (maximum aka white level)
+    input       logic       [SYS_REG_WIDTH - 1 : 0  ]       requested_level_black       ,   // Requested level of the black color (minimum aka black level)
+    input       logic       [SYS_REG_WIDTH - 1 : 0  ]       requested_level_intermediate,   // Requested level of the intermediate color (any desired level)
+    input       logic       [SYS_REG_WIDTH - 1 : 0  ]       requested_horizontal_step   ,   // Requested step of the generation for rows
+    input       logic       [SYS_REG_WIDTH - 1 : 0  ]       requested_vertical_step     ,   // Requested step of the generation for cols
 
     //Output signals
     output		logic		                                sig_out_vsync               ,   //V-Sync active-high signal for syncronization
@@ -35,21 +37,60 @@ module video_pattern_generator_wrapper
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //Begin of local signals and parameters section
 
-//Registers to latch
+//Registers to latch to setup the generator
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_cols_gen_reg              ;   // Reg for the requested number of colomns to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_gen_reg              ;   // Reg for the requested number of rows to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_cols_del_before_reg       ;   // Reg for the requested number of colomns to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_del_before_reg       ;   // Reg for the requested number of rows to be generateds
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_cols_del_after_reg        ;   // Reg for the requested number of colomns to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_del_after_reg        ;   // Reg for the requested number of rows to be generateds
-logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_del_after_reg        ;   // Reg for the requested number of rows to be generateds
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_v_sync_duration_reg       ;   // Reg for the requested number of rows to be generateds
 logic [3                 : 0  ]     requested_data_pattern_reg          ;   // Requested pattren type to be generated
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_level_white_reg           ;   // Requested level of the color (maximum aka white level)
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_level_black_reg           ;   // Requested level of the color (minimum aka black level)
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_level_intermediate_reg    ;   // Requested level of the intermediate color (any desired level)
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_horizontal_step_reg       ;   // Requested step of the generation for rows
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_vertical_step_reg         ;   // Requested step of the generation for cols
+
+
+//Enum variable for the pattern types (better readabilty)
+enum 	logic 	    [3 : 0] 	    {
+                                        ALL_BLACK                       ,
+                                        ALL_WHITE                       ,
+                                        ALL_INTERMEDIATE                ,
+                                        CHECKER_ROWS                    ,
+                                        CHECKER_COLS                    ,
+                                        CHECKER_IMAGE                   ,
+                                        GRADIENT_HORIZONTAL             ,
+                                        GRADIENT_VERTICAL               ,
+                                        GRADIENT_XORED      
+                                    } 
+                                        available_patterns_enum         ;
+
+/*
+Table of the patterns which can be requested to generate the test video
+PATTERN NUMBER      NAME                    DEFINITION
+0                   ALL_BLACK               Generate all pixels as black pixels (requested_level_black)
+1                   ALL_WHITE               Generate all pixels as white pixels (requested_level_white)
+2                   ALL_INTERMEDIATE        Generate all pixels as intermediate level (requested_level_intermediate)
+3                   CHECKER_ROWS            Generate rows as black-white-black-white-etc levels with the selected horizontal step
+4                   CHECKER_COLS            Generate cols as black-white-black-white-etc levels with the selected vertical step
+5                   CHECKER_IMAGE           Generate the checker board with the selected horizontal adn vertical steps for the correlated sizes of the tiles
+6                   GRADIENT_HORIZONTAL     Generate horizontal gradient pattern
+7                   GRADIENT_VERTICAL       Generate vertical gradient pattern
+8                   GRADIENT_XORED          Generate XORed gradient pattern
+*/
+
 
 //Counters to generate patterns
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_cols_gen_cnt              ;   // Counter for the requested number of colomns to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_gen_cnt              ;   // Counter for the requested number of rows to be generated
 logic [SYS_REG_WIDTH - 1 : 0  ]     requested_v_sync_duration_cnt       ;   // Counter for the requested number of rows to be generated
+
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_rows_step_counter         ;   // Counter for the requested number of rows to be generated
+logic [SYS_REG_WIDTH - 1 : 0  ]     requested_cols_step_counter         ;   // Counter for the requested number of rows to be generated
+logic                               requested_rows_step_flag            ;   // Counter for the requested number of rows to be generated
+logic                               requested_cols_step_flag            ;   // Counter for the requested number of rows to be generated
 
 //End of local signals and parameters section
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -58,37 +99,16 @@ logic [SYS_REG_WIDTH - 1 : 0  ]     requested_v_sync_duration_cnt       ;   // C
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //Begin of checking input parameters secntion section
 initial begin
-    if(CNT_COLS <= 0) begin
-        $error("Parameter CNT_COLS must NOT be equal or less than 0");
+    if(SYS_REG_WIDTH <= 0) begin
+        $error("Parameter SYS_REG_WIDTH must NOT be equal or less than 0");
     end
 
-    if(CNT_ROWS <= 0) begin
-        $error("Parameter CNT_ROWS must NOT be equal or less than 0");
+    if(OUT_DATA_WIDTH <= 0) begin
+        $error("Parameter OUT_DATA_WIDTH must NOT be equal or less than 0");
     end
 
-    if(CNT_COLS_DEL_BEFORE == 0) begin
-        $warning("CNT_COLS_DEL_BEFORE == 0, no additional pipe will be used for output signal");
-    end
-
-    if(CNT_ROWS_DEL_BEFORE == 0) begin
-        $warning("CNT_ROWS_DEL_BEFORE == 0, no additional pipe will be used for output signal");
-    end
-
-    if(CNT_COLS_DEL_AFTER == 0) begin
-        $warning("CNT_COLS_DEL_AFTER == 0, no additional pipe will be used for output signal");
-    end
-
-    if(CNT_ROWS_DEL_AFTER == 0) begin
-        $warning("CNT_ROWS_DEL_AFTER == 0, no additional pipe will be used for output signal");
-    end
-
-    $display("%m setup with parameter CNT_COLS              : %d", CNT_COLS             );
-    $display("%m setup with parameter CNT_ROWS              : %d", CNT_ROWS             );
-    $display("%m setup with parameter CNT_COLS_DEL_BEFORE   : %d", CNT_COLS_DEL_BEFORE  );
-    $display("%m setup with parameter CNT_ROWS_DEL_BEFORE   : %d", CNT_ROWS_DEL_BEFORE  );
-    $display("%m setup with parameter CNT_COLS_DEL_AFTER    : %d", CNT_COLS_DEL_AFTER   );
-    $display("%m setup with parameter CNT_ROWS_DEL_AFTER    : %d", CNT_ROWS_DEL_AFTER   );
-    $display("%m setup with parameter IS_RST_SYNC           : %d", IS_RST_SYNC          );
+    $display("%m setup with parameter SYS_REG_WIDTH         : %d", SYS_REG_WIDTH    );
+    $display("%m setup with parameter OUT_DATA_WIDTH        : %d", OUT_DATA_WIDTH   );
 end
 //End of checking input parameters secntion section
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -100,14 +120,19 @@ always_ff @(posedge clk)
 begin
     if(!enable)
         begin
-            requested_cols_gen_reg          <=  requested_cols_gen              ;
-            requested_rows_gen_reg          <=  requested_rows_gen              ;
-            requested_cols_del_before_reg   <=  requested_cols_del_before       ;
-            requested_rows_del_before_reg   <=  requested_rows_del_before       ;
-            requested_cols_del_after_reg    <=  requested_cols_del_after        ;
-            requested_rows_del_after_reg    <=  requested_rows_del_after        ;
-            requested_v_sync_duration_reg   <=  requested_v_sync_duration       ;
-            requested_data_pattern_reg      <=  requested_data_pattern          ;
+            requested_cols_gen_reg              <=  requested_cols_gen              ;
+            requested_rows_gen_reg              <=  requested_rows_gen              ;
+            requested_cols_del_before_reg       <=  requested_cols_del_before       ;
+            requested_rows_del_before_reg       <=  requested_rows_del_before       ;
+            requested_cols_del_after_reg        <=  requested_cols_del_after        ;
+            requested_rows_del_after_reg        <=  requested_rows_del_after        ;
+            requested_v_sync_duration_reg       <=  requested_v_sync_duration       ;
+            requested_data_pattern_reg          <=  requested_data_pattern          ;
+            requested_level_white_reg           <=  requested_level_white_reg       ;
+            requested_level_black_reg           <=  requested_level_black_reg       ;
+            requested_level_intermediate_reg    <=  requested_level_intermediate    ;
+            requested_horizontal_step_reg       <=  requested_horizontal_step       ;
+            requested_vertical_step_reg         <=  requested_vertical_step         ;
         end
 end
 //End of driving counters section
@@ -208,6 +233,75 @@ end
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
+
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+//Begin of driving counters to generate pattern steps etc section
+
+//Horizontal counter
+always_ff @(posedge clk)
+begin
+    if(!enable)
+        begin
+            requested_rows_step_counter <= '0;
+            requested_rows_step_flag    <= '0;
+        end
+    else
+        begin
+            if ((requested_rows_gen_cnt >= requested_rows_del_before_reg) && 
+                (requested_rows_gen_cnt < requested_rows_del_before_reg + requested_rows_gen_reg)) begin
+                    
+                    if(requested_cols_gen_cnt == requested_cols_del_before_reg + requested_cols_gen_reg + requested_cols_del_after_reg - 1) begin
+                        if (requested_rows_step_counter == requested_horizontal_step_reg - 1) begin
+                            requested_rows_step_counter <= '0;
+                            requested_rows_step_flag    <= ~requested_rows_step_flag;
+                        end
+                        else begin
+                            requested_rows_step_counter <= requested_rows_step_counter + 1;
+                        end
+                    end
+            end
+            else begin
+                requested_rows_step_counter <= '0;
+                requested_rows_step_flag    <= '0;
+            end
+        end
+end
+
+//Vertical counter
+always_ff @(posedge clk)
+begin
+    if(!enable)
+        begin
+            requested_cols_step_counter <= '0;
+            requested_cols_step_flag    <= '0;
+        end
+    else
+        begin
+            if ((requested_rows_gen_cnt >= requested_rows_del_before_reg) && 
+                (requested_rows_gen_cnt < requested_rows_del_before_reg + requested_rows_gen_reg)) begin
+
+                    if((requested_cols_gen_cnt >= requested_cols_del_before_reg) && 
+                        (requested_cols_gen_cnt < requested_cols_del_before_reg + requested_cols_gen_reg))begin
+                        
+                        if(requested_cols_gen_cnt == requested_vertical_step_reg - 1) begin
+                            requested_cols_step_counter <= '0;
+                            requested_cols_step_flag    <= ~requested_cols_step_flag;
+                        end
+                        else begin
+                            requested_cols_step_counter <= requested_cols_step_counter + 1;
+                        end
+                    end
+                    else begin
+                        requested_cols_step_counter <= '0;
+                        requested_cols_step_flag    <= '0;
+                    end
+            end
+        end
+end
+//End of driving counters to generate pattern steps etc section
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 //Begin of data pattern generation section
 always_ff @(posedge clk)
@@ -219,13 +313,24 @@ begin
     else
         begin
             case (requested_data_pattern_reg)
-                INSERTCASE:
+                ALL_BLACK:          sig_out_data <= requested_level_black_reg                                                                           ;
+                ALL_WHITE:          sig_out_data <= requested_level_white_reg                                                                           ;
+                ALL_INTERMEDIATE:   sig_out_data <= requested_level_intermediate_reg                                                                    ;
+                CHECKER_ROWS:       sig_out_data <= requested_rows_step_flag ? requested_level_white_reg : requested_level_black_reg                    ;
+                CHECKER_COLS:       sig_out_data <= requested_cols_step_flag ? requested_level_white_reg : requested_level_black_reg                    ;
+                CHECKER_IMAGE:
                     begin
-                        
+                        if(requested_rows_step_flag)
+                            sig_out_data <= requested_cols_step_flag ? requested_level_white_reg : requested_level_black_reg                            ; 
+                        else
+                            sig_out_data <= requested_cols_step_flag ? requested_level_black_reg : requested_level_white_reg                            ; 
                     end
-                default:
+                GRADIENT_HORIZONTAL: sig_out_data <= requested_cols_step_counter[OUT_DATA_WIDTH-1:0]                                                    ;
+                GRADIENT_VERTICAL:  sig_out_data <= requested_rows_step_counter[OUT_DATA_WIDTH-1:0]                                                     ;
+                GRADIENT_XORED: sig_out_data <= requested_cols_step_counter[OUT_DATA_WIDTH-1:0] ^ requested_rows_step_counter[OUT_DATA_WIDTH-1:0]       ;
+                default: //If no corret pattern found - generating black level
                     begin
-                        
+                        sig_out_data <= requested_level_black_reg                                                                                       ;
                     end
             endcase
         end
